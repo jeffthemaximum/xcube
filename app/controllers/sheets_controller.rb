5a82f6242326e6e819927a812a16df3466a6d76d
jeffthemaximum <frey.maxim@gmail.com>
2016-08-29 12:38:28 -0400

    adds functionality to accept sheet name

--- app/controllers/sheets_controller.rb
+++ app/controllers/sheets_controller.rb
@@ -1,9 +1,16 @@
 class SheetsController < ApplicationController
   def create
-    binding.pry
+    url = Rails.application.secrets.helloworld_xcube_url
+    sheet_name = params["sheet"]["name"]
+    HTTParty.post url , body: { s: sheet_name }, timeout: 300
   end
 
   def new
     @sheet = Sheet.new
   end
+
+  private
+    def sheet_params
+      params.permit(params["sheet"])
+    end
 end
